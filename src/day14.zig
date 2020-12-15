a77af794d3d0e3775cc0cdd7af03b6e7bdf50d38
voidumbrella <voidumbrella@gmail.com>
2020-12-15 01:03:02 -0500

    bad commit message #1

--- src/day14.zig
+++ src/day14.zig
@@ -11,20 +11,14 @@ const input = @embedFile("../input/day14_input.txt");
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 const allocator = &arena.allocator;
 
-fn write(mem: *Map(usize, u64), mask: []const u8, addr: usize, val: u64, start: u6) void {
-    var i: u6 = start;
-    while (i < 36) : (i += 1) {
-        const c = mask[35 - i];
-        if (c == 'X') {
-            const cleared = addr & ~(@as(u64, 1) << i);
-            const set = addr | @as(u64, 1) << i;
-            mem.put(cleared, val) catch unreachable;
-            mem.put(set, val) catch unreachable;
-            write(mem, mask, cleared, val, i + 1);
-            write(mem, mask, set, val, i + 1);
-            return;
-        }
-    }
+fn write(mem: *Map(usize, u64), float_bits: []const u6, addr: usize, val: u64) void {
+    if (float_bits.len == 0) return;
+    const cleared = addr & ~(@as(u64, 1) << float_bits[0]);
+    const set = addr | @as(u64, 1) << float_bits[0];
+    mem.put(cleared, val) catch unreachable;
+    mem.put(set, val) catch unreachable;
+    write(mem, float_bits[1..], cleared, val);
+    write(mem, float_bits[1..], set, val);
 }
 
 pub fn main() !void {
@@ -46,6 +40,8 @@ pub fn main() !void {
             const addr = try std.fmt.parseInt(usize, part_iterator.next().?, 10);
             const val = try std.fmt.parseInt(u64, part_iterator.next().?, 10);
 
+            var float_bits = ArrayList(u6).init(allocator);
+
             // Part 1
             var masked_addr = addr;
             var masked_val = val;
@@ -57,12 +53,14 @@ pub fn main() !void {
                 } else if (c == '1') {
                     masked_val |= @as(u64, 1) << i;
                     masked_addr |= @as(u64, 1) << i;
+                } else if (c == 'X') {
+                    try float_bits.append(i);
                 }
             }
             try part1_mem.put(addr, masked_val);
 
             // Part 2
-            write(&part2_mem, mask, masked_addr, val, 0);
+            write(&part2_mem, float_bits.items, masked_addr, val);
         }
     }
 
--- src/day15.zig
+++ src/day15.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const ArrayList = std.ArrayList;
+const Map = std.AutoHashMap;
 const Allocator = std.mem.Allocator;
 const Timer = std.time.Timer;
 const assert = std.debug.assert;
@@ -13,14 +14,50 @@ const allocator = &arena.allocator;
 pub fn main() !void {
     var timer = try Timer.start();
 
-    var line_iterator = std.mem.tokenize(input, "\n");
+    var part_iterator = std.mem.tokenize(input, ",\n");
 
     var part1_ans: u64 = 0;
     var part2_ans: u64 = 0;
 
-    while (line_iterator.next()) |line| {
-        //
+    var numbers = Map(u64, u64).init(allocator);
+
+    var last_said: u64 = undefined;
+    var age: u64 = 0;
+    var turn: u64 = 1;
+
+    // Say the starting numbers first
+    while (part_iterator.next()) |part| {
+        last_said = try std.fmt.parseInt(u64, part, 10);
+        try numbers.put(last_said, turn);
+        turn += 1;
     }
+
+    while (turn < 2020) : (turn += 1) {
+        // We are going to say the age of the last number,
+        // but before doing that let's be nice for the next person
+        // and find when this age was last said
+        const new_age = if (numbers.get(age)) |prev_turn|
+            turn - prev_turn
+        else
+            0;
+
+        try numbers.put(age, turn);
+        age = new_age;
+        last_said = age;
+    }
+    part1_ans = last_said;
+
+    while (turn < 30000000) : (turn += 1) {
+        const new_age = if (numbers.get(age)) |prev_turn|
+            turn - prev_turn
+        else
+            0;
+
+        try numbers.put(age, turn);
+        age = new_age;
+        last_said = age;
+    }
+    part1_ans = last_said;
 
     print("=== Day 15 === ({} µs)\n", .{timer.lap() / 1000});
     print("Part 1: {}\nPart 2: {}\n", .{ part1_ans, part2_ans });
