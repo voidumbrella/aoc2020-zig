7536b59e3799beacf5425916bc756079995f16c7
voidumbrella <voidumbrella@gmail.com>
2020-12-22 01:33:13 -0500

    asdf

--- src/day20.zig
+++ src/day20.zig
@@ -10,17 +10,200 @@ const input = @embedFile("../input/day20_input.txt");
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 const allocator = &arena.allocator;
 
+const Border = enum {
+    top, bottom, left, right
+};
+
+const Tile = struct {
+    id: u64,
+
+    p: [10][10]u8 = [_][10]u8{[_]u8{'.'} ** 10} ** 10,
+    fixed: bool = false,
+
+    fn print(self: Tile) void {
+        print("{}: {}\n", .{ self.id, self.fixed });
+        for (self.p) |row| {
+            for (row) |c| {
+                print("{c}", .{c});
+            }
+            print("\n", .{});
+        }
+    }
+
+    fn rotate(self: *Tile) void {
+        var new = [_][10]u8{[_]u8{'.'} ** 10} ** 10;
+        var i: usize = 0;
+        while (i < 10) : (i += 1) {
+            var j: usize = 0;
+            while (j < 10) : (j += 1) {
+                new[i][j] = self.p[9 - j][i];
+            }
+        }
+
+        i = 0;
+        while (i < 10) : (i += 1) {
+            std.mem.copy(u8, &self.p[i], &new[i]);
+        }
+    }
+
+    fn flipX(self: *Tile) void {
+        var i: usize = 0;
+        while (i < 10) : (i += 1) {
+            var j: usize = 0;
+            while (j < 5) : (j += 1) {
+                var tmp = self.p[i][j];
+                self.p[i][j] = self.p[i][9 - j];
+                self.p[i][9 - j] = tmp;
+            }
+        }
+    }
+
+    fn flipY(self: *Tile) void {
+        var i: usize = 0;
+        while (i < 5) : (i += 1) {
+            var j: usize = 0;
+            while (j < 10) : (j += 1) {
+                var tmp = self.p[i][j];
+                self.p[i][j] = self.p[9 - i][j];
+                self.p[9 - i][j] = tmp;
+            }
+        }
+    }
+
+    fn match(self: *const Tile, other: *const Tile) ?Border {
+        if (std.mem.eql(u8, &self.p[0], &other.p[9])) return Border.top;
+        if (std.mem.eql(u8, &self.p[9], &other.p[0])) return Border.bottom;
+
+        var i: usize = 0;
+        var left: bool = true;
+        var right: bool = true;
+        while (i < 10) : (i += 1) {
+            if (self.p[i][0] != other.p[i][9]) left = false;
+            if (self.p[i][9] != other.p[i][0]) right = false;
+        }
+        if (left) return Border.left;
+        if (right) return Border.right;
+
+        return null;
+    }
+
+    fn findMatch(self: *Tile, other: *Tile) ?Border {
+        assert(!self.fixed);
+        var rots: u8 = 0;
+        var found: ?Border = null;
+        while (rots < 4) : (rots += 1) {
+            if (self.match(other)) |b| {
+                found = b;
+                break;
+            }
+            self.rotate();
+        }
+
+        return found;
+    }
+};
+
+const Image = struct {
+    p: [96][96]u8 = [_][96]u8{[_]u8{'.'} ** 96} ** 96,
+
+    fn place(self: *Image, x: usize, y: usize, tile: Tile) void {
+        assert(x < 12 and y < 12);
+        tile.print();
+        print("\n", .{});
+
+        for (tile.p[1..10]) |row, i| {
+            for (row[1..10]) |c, j| {
+                self.p[y * 8 + i][x * 8 + j] = c;
+            }
+        }
+    }
+
+    fn print(self: Image) void {
+        for (self.p) |row| {
+            for (row) |c| {
+                print("{c}", .{c});
+            }
+            print("\n", .{});
+        }
+    }
+};
+
+fn eql(a: []const u8, b: []const u8) bool {
+    assert(a.len == b.len);
+    for (a) |c, i|
+        if (c != b[i]) return false;
+    return true;
+}
+
+fn reql(a: []const u8, b: []const u8) bool {
+    assert(a.len == b.len);
+    for (a) |c, i|
+        if (c != b[b.len - 1 - i]) return false;
+    return true;
+}
+
 pub fn main() !void {
     var timer = try Timer.start();
 
-    var line_iterator = std.mem.tokenize(input, "\n");
+    var tile_iterator = std.mem.split(input, "\n\n");
 
-    var part1_ans: u64 = 0;
-    var part2_ans: u64 = 0;
+    var tiles = ArrayList(Tile).init(allocator);
 
-    while (line_iterator.next()) |line| {
-        //
+    while (tile_iterator.next()) |tile_str| {
+        var line_iterator = std.mem.tokenize(tile_str, " :\n");
+        _ = line_iterator.next().?; // "Tile "
+        const id = try std.fmt.parseInt(u64, line_iterator.next().?, 0);
+
+        var i: usize = 0;
+
+        var tile = Tile{ .id = id };
+
+        while (line_iterator.next()) |line| {
+            std.mem.copy(u8, &tile.p[i], line);
+            i += 1;
+        }
+
+        try tiles.append(tile);
     }
+
+    var image = Image{};
+
+    var part1_ans: u64 = 1;
+
+    // Find corner pieces for part 1, and determine right orientation for each tile
+    var i: usize = 0;
+    while (i < tiles.items.len) : (i += 1) {
+        var self = &tiles.items[i];
+
+        var j: usize = 0;
+        while (j < tiles.items.len) : (j += 1) {
+            if (j == i) continue;
+
+            var other = &tiles.items[j];
+        }
+    }
+
+    // Finding upper-left corner
+    for (tiles.items) |self| {
+        // var top = false;
+        // var left = false;
+        // var right = false;
+        // var bottom = false;
+        // for (tiles.items) |other| {
+        //     if (other.id == self.id) continue;
+
+        //     if (self.match(&other)) |b| {
+        //         if (b == Border.top) top = true;
+        //         if (b == Border.left) left = true;
+        //         if (b == Border.right) right = true;
+        //         if (b == Border.bottom) bottom = true;
+        //     }
+        // }
+        // if (!right and !bottom and !top and !left)
+        //     image.place(0, 0, self);
+    }
+
+    var part2_ans: u64 = 0;
 
     print("=== Day 20 === ({} µs)\n", .{timer.lap() / 1000});
     print("Part 1: {}\nPart 2: {}\n", .{ part1_ans, part2_ans });
--- src/day22.zig
+++ src/day22.zig
@@ -10,17 +10,100 @@ const input = @embedFile("../input/day22_input.txt");
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 const allocator = &arena.allocator;
 
+fn score(deck: *ArrayList(u32)) u32 {
+    var res: u32 = 0;
+    var i: u32 = @intCast(u32, deck.items.len);
+    for (deck.items) |n| {
+        res += n * i;
+        i -= 1;
+    }
+    return res;
+}
+
+// Returns true if player 1 wins
+fn war(p1_deck: *ArrayList(u32), p2_deck: *ArrayList(u32)) bool {
+    while (p1_deck.items.len != 0 and p2_deck.items.len != 0) {
+        const p1 = p1_deck.orderedRemove(0);
+        const p2 = p2_deck.orderedRemove(0);
+        if (p1 > p2) {
+            p1_deck.append(p1) catch unreachable;
+            p1_deck.append(p2) catch unreachable;
+        } else {
+            p2_deck.append(p2) catch unreachable;
+            p2_deck.append(p1) catch unreachable;
+        }
+    }
+    return p1_deck.items.len != 0;
+}
+
+const Record = struct { p1: ArrayList(u32), p2: ArrayList(u32) };
+
+// Returns true if player 1 wins
+fn rec_war(p1_deck: *ArrayList(u32), p2_deck: *ArrayList(u32)) bool {
+    var history = std.AutoHashMap(u32, void).init(allocator);
+
+    while (p1_deck.items.len != 0 and p2_deck.items.len != 0) {
+        var p1_wins: bool = undefined;
+
+        const r = score(p1_deck) * score(p2_deck);
+        if (history.get(r) != null) {
+            return true;
+        }
+        history.put(r, .{}) catch unreachable;
+
+        const p1 = p1_deck.orderedRemove(0);
+        const p2 = p2_deck.orderedRemove(0);
+
+        if (p1 <= p1_deck.items.len and p2 <= p2_deck.items.len) {
+            var new_p1 = ArrayList(u32).init(allocator);
+            var new_p2 = ArrayList(u32).init(allocator);
+            new_p1.appendSlice(p1_deck.items[0..p1]) catch unreachable;
+            new_p2.appendSlice(p2_deck.items[0..p2]) catch unreachable;
+            p1_wins = rec_war(&new_p1, &new_p2);
+        } else {
+            p1_wins = p1 > p2;
+        }
+
+        if (p1_wins) {
+            p1_deck.append(p1) catch unreachable;
+            p1_deck.append(p2) catch unreachable;
+        } else {
+            p2_deck.append(p2) catch unreachable;
+            p2_deck.append(p1) catch unreachable;
+        }
+    }
+    return p1_deck.items.len != 0;
+}
+
 pub fn main() !void {
     var timer = try Timer.start();
 
-    var line_iterator = std.mem.tokenize(input, "\n");
+    var line_iterator = std.mem.tokenize(input, ":\n");
 
-    var part1_ans: u64 = 0;
-    var part2_ans: u64 = 0;
+    var p1_deck = ArrayList(u32).init(allocator);
+    var p2_deck = ArrayList(u32).init(allocator);
 
+    _ = line_iterator.next();
     while (line_iterator.next()) |line| {
-        //
+        if (line[0] == 'P') break;
+        const n = try std.fmt.parseInt(u32, line, 10);
+        try p1_deck.append(n);
     }
+    while (line_iterator.next()) |line| {
+        const n = try std.fmt.parseInt(u32, line, 10);
+        try p2_deck.append(n);
+    }
+
+    var p1_copy = ArrayList(u32).init(allocator);
+    var p2_copy = ArrayList(u32).init(allocator);
+    p1_copy.appendSlice(p1_deck.items[0..]) catch unreachable;
+    p2_copy.appendSlice(p2_deck.items[0..]) catch unreachable;
+
+    var winner = if (war(&p1_deck, &p2_deck)) p1_deck else p2_deck;
+    const part1_ans = score(&winner);
+
+    winner = if (rec_war(&p1_copy, &p2_copy)) p1_copy else p2_copy;
+    const part2_ans = score(&winner);
 
     print("=== Day 22 === ({} µs)\n", .{timer.lap() / 1000});
     print("Part 1: {}\nPart 2: {}\n", .{ part1_ans, part2_ans });
